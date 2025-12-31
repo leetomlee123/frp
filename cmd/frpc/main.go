c55d72423028b4a166c3eedba864e67c106dd8af
leetomlee123 <leetomlee123@gmail.com>
2025-12-31 16:54:32 +0800

    set frpc package to mobile

--- cmd/frpc/main.go
+++ cmd/frpc/main.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package mobile
 
 import (
 	_ "github.com/fatedier/frp/assets/frpc"
